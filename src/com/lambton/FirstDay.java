dc48ed0bff6c04b0ff745793b285f2656ea8e1cd
CHARAN <charanpreetkaur026@gmail.com>
2019-09-18 19:56:22 -0400

    intial commit

---
+++ src/com/lambton/FirstDay.java
@@ -0,0 +1,20 @@
+package com.lambton;
+
+public class FirstDay
+{
+    public static void main(String[] args)
+    {
+        System.out.println("charan");
+    int a, b,c;
+    a=5;
+    b=7;
+    c= a + b;
+    System.out.println("sum:" + c);
+
+    }
+    public class Student
+    {
+        int studentID;
+        String studentName;
+    }
+}
